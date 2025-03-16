d42aa91daa02ef220f95031a963f5eccfa5eb703
ManishNewa <manishnewar26@gmail.com>
2025-03-16 20:55:50 +0000

    setup configured to handle vercel

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ dotenv.config();
 const env = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(
-        `Server is running on ${(env === 'development' ? process.env.API_URL : '') + ':' + PORT}`
-    );
+app.get("/", (req, res) => {
+    res.send("Node.js API running on Vercel!");
 });
+
+
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+        console.log(
+            `Server is running on ${(env === 'development' ? process.env.API_URL : '') + ':' + PORT}`
+        );
+    });
+}
