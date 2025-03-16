4bf3a794626e1da79a55f762b9cbe68712e3ab6b
ManishNewa <manishnewar26@gmail.com>
2025-03-16 19:28:19 +0000

    removed path to set it with default way

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '/.env' });
+dotenv.config();
 
 const env = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 3000;
