fc752fea852338b4655986dad4d9fb926cb60050
ManishNewa <manishnewar26@gmail.com>
2025-03-16 19:28:49 +0000

    api added to app js file

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import api from './src/api.js'
+
 const app = express();
 
 dotenv.config({ path: './.env' });
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api', api)
 
 export default app;
